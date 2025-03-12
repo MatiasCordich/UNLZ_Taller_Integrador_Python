0b70ee2da17ec02d9d50e398eddf3fe61e809371
MatiasCordich <m.scordich@gmail.com>
2025-03-11 23:30:11 -0300

    Se agregó el siguiente tema: 07-Set y Diccionarios

---
+++ 07-Set y Diccionarios/01-Set.py
@@ -0,0 +1,12 @@
+# ------------------------- [SET] -------------------------
+'''
+Es otra estructura de datos similar a las listas pero con la diferencia de que los sets no tienen ÍNDICE ni ORDEN. 
+'''
+
+# Ejemplo - Definicion de un set
+
+pistas = {"Monza", "Monaco", "Spa Francorchamps"}
+
+# Los 
+print(type(pistas))
+print(pistas)
