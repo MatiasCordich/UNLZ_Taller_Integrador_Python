f9988ac051e2b57b7db2257545060e7342af4531
MatiasCordich <m.scordich@gmail.com>
2025-03-12 00:02:11 -0300

    Se agregaron más ejemplos

--- 07-Set y Diccionarios/01-Set.py
+++ 07-Set y Diccionarios/01-Set.py
@@ -7,6 +7,20 @@ Es otra estructura de datos similar a las listas pero con la diferencia de que l
 
 pistas = {"Monza", "Monaco", "Spa Francorchamps"}
 
-# Los 
 print(type(pistas))
+
+'''
+Como no tienen un orden ni índice, cuando lo muestre por pantalla los elementos van a aparecer en un orden aleatorio cada vez qe ejecute el programa. 
+'''
 print(pistas)
+
+# Metodos de los set
+'''
+Me agrega este elemento al set y me lo agrega en cualquier posición
+'''
+pistas.add("Silverstone")
+
+'''
+Como mencionamos, al no tener índice se le debe pasar por parámetro un valor par que lo vaya a buscar al set y lo elemine
+'''
+pistas.remove("Spa Francorchamps")
